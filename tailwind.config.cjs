48d011da9bfb6386af0f0ab1608e06c83436cb28
Mehdi Zibout <mehdizibout@gmail.com>
2023-01-16 17:07:37 +0100

    Init, tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Space Grotesk", "sans-serif"],
+    },
+    colors: {
+      black: "#000",
+      white: "#FFF",
+      "dark-purple": "#5C2DD5",
+      purple: "#7945FF",
+      red: "#FD6687",
+      yellow: "#FFCE67",
+    },
+    fontSize: {
+      hl: ["3.5rem", { fontWeight: 700, lineHeight: "4.43rem" }],
+      hm: ["1.5rem", { fontWeight: 700, lineHeight: "1.93rem" }],
+      hs: ["1.25rem", { fontWeight: 700, lineHeight: "1.625rem" }],
+      hxs: ["1rem", { fontWeight: 700, lineHeight: "1.31rem" }],
+      body: ["1rem", { fontWeight: 500, lineHeight: "1.31rem" }],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
